f1557ecbe36e2ec0b7cc4eb44bb54b8b37149ebf
tomj <tarjohnson1979@gmail.com>
2017-08-26 18:32:22 +0100

    Same reordering, in the program builder

--- src/main/java/com/writeoncereadmany/minimalang/ast/ProgramBuilder.java
+++ src/main/java/com/writeoncereadmany/minimalang/ast/ProgramBuilder.java
@@ -23,42 +23,44 @@ public interface ProgramBuilder {
 
     static Expression buildExpression(MinimalangParser.ExpressionContext expression) {
         return matchValue(expression,
-                ifType(MinimalangParser.CallContext.class, call ->
-                    call(buildExpression(
-                        call.expression(0)),
-                        call.expression()
-                            .subList(1, call.expression().size())
-                            .stream()
-                            .map(ProgramBuilder::buildExpression)
-                            .collect(toList()))),
-                ifType(MinimalangParser.VariableContext.class, var ->
-                    variable(var.IDENTIFIER().getText())),
-                ifType(MinimalangParser.StringContext.class, str ->
-                    stringLiteral(stripSurroundingQuotes(str.STRING_LITERAL().getText()))),
+            ifType(MinimalangParser.StringContext.class, str ->
+                stringLiteral(stripSurroundingQuotes(str.STRING_LITERAL().getText()))),
             ifType(MinimalangParser.NumberContext.class, num ->
-                    numberLiteral(num.NUMBER_LITERAL().getText())),
-                ifType(MinimalangParser.SequenceContext.class, seq ->
-                    sequence(seq.expression()
+                numberLiteral(num.NUMBER_LITERAL().getText())),
+            ifType(MinimalangParser.DeclarationContext.class, dec ->
+                declaration(
+                    dec.IDENTIFIER().getText(),
+                    buildExpression(dec.expression()))),
+            ifType(MinimalangParser.VariableContext.class, var ->
+                variable(var.IDENTIFIER().getText())),
+            ifType(MinimalangParser.ObjectContext.class, obj ->
+                objectLiteral(HigherOrderFunctions.zip(
+                    obj.IDENTIFIER().stream().map(ParseTree::getText),
+                    obj.expression().stream().map(exp -> buildExpression(exp))).collect(toList()))),
+            ifType(MinimalangParser.AccessContext.class, acc ->
+                access(
+                    buildExpression(acc.expression()),
+                    acc.IDENTIFIER().getText())),
+            ifType(MinimalangParser.FunctionContext.class, fun ->
+                function(
+                    fun.IDENTIFIER().stream().map(ParseTree::getText).collect(toList()),
+                    buildExpression(fun.expression())
+                )),
+            ifType(MinimalangParser.CallContext.class, call ->
+                call(buildExpression(
+                    call.expression(0)),
+                    call.expression()
+                        .subList(1, call.expression().size())
                         .stream()
                         .map(ProgramBuilder::buildExpression)
                         .collect(toList()))),
-                ifType(MinimalangParser.DeclarationContext.class, dec ->
-                    declaration(
-                        dec.IDENTIFIER().getText(),
-                        buildExpression(dec.expression()))),
-                ifType(MinimalangParser.ObjectContext.class, obj ->
-                    objectLiteral(HigherOrderFunctions.zip(
-                        obj.IDENTIFIER().stream().map(ParseTree::getText),
-                        obj.expression().stream().map(exp -> buildExpression(exp))).collect(toList()))),
-                ifType(MinimalangParser.AccessContext.class, acc ->
-                    access(
-                        buildExpression(acc.expression()),
-                        acc.IDENTIFIER().getText())),
-                ifType(MinimalangParser.FunctionContext.class, fun ->
-                    function(
-                        fun.IDENTIFIER().stream().map(ParseTree::getText).collect(toList()),
-                        buildExpression(fun.expression())
-                    ))
-        ).otherwise(exp -> { throw new RuntimeException("Failed to find an implementation for " + exp.getClass()); });
+            ifType(MinimalangParser.SequenceContext.class, seq ->
+                sequence(seq.expression()
+                    .stream()
+                    .map(ProgramBuilder::buildExpression)
+                    .collect(toList())))
+        ).otherwise(exp -> {
+            throw new RuntimeException("Failed to find an implementation for " + exp.getClass());
+        });
     }
 }
