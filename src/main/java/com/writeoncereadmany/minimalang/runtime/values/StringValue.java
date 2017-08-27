1e40d92b4c9f038f2a28d5f90a51ffa661883aa2
tomj <tarjohnson1979@gmail.com>
2017-08-27 16:02:59 +0100

    Add string concatenation

--- src/main/java/com/writeoncereadmany/minimalang/runtime/values/StringValue.java
+++ src/main/java/com/writeoncereadmany/minimalang/runtime/values/StringValue.java
@@ -1,13 +1,25 @@
 package com.writeoncereadmany.minimalang.runtime.values;
 
+import com.writeoncereadmany.minimalang.runtime.values.prelude.BuiltinFunction;
+import com.writeoncereadmany.minimalang.runtime.values.prelude.Extractors;
+
+import static co.unruly.control.pair.Maps.entry;
+import static co.unruly.control.pair.Maps.mapOf;
+
 /**
  * Created by tomj on 22/08/2017.
  */
-public class StringValue implements Value {
+public class StringValue extends ObjectValue {
 
     public final String text;
 
     public StringValue(String text) {
+        super(mapOf(
+            entry("concat",
+                new BuiltinFunction<>(
+                    "String:concat[]",
+                    Extractors.singleParamOfType(StringValue.class),
+                    (string) -> new StringValue(text.concat(string.text))))));
         this.text = text;
     }
 
---
+++ src/test/java/com/writeoncereadmany/minimalang/StringsTest.java
@@ -0,0 +1,30 @@
+package com.writeoncereadmany.minimalang;
+
+import com.writeoncereadmany.minimalang.ast.Program;
+import com.writeoncereadmany.minimalang.runtime.Environment;
+import com.writeoncereadmany.minimalang.runtime.values.prelude.PrintFunction;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.writeoncereadmany.minimalang.runtime.Evaluator.evaluator;
+import static org.hamcrest.Matchers.hasItems;
+import static org.junit.Assert.assertThat;
+
+public class StringsTest {
+
+    public Compiler compiler = new Compiler();
+
+    private final List<String> printed = new ArrayList<>();
+    private final Environment builtins = new Environment()
+        .with("print", new PrintFunction(printed::add));
+
+    @Test
+    public void canConcatenateStrings() throws Exception {
+        Program program = compiler.compile("print['Hello, ':concat['World!']]");
+        program.run(evaluator(), builtins);
+
+        assertThat(printed, hasItems("Hello, World!"));
+    }
+}
