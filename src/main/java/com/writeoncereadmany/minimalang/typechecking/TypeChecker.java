fa9da7d49fee949dc13c5b97e1eba98fb9acefe2
tomj <tarjohnson1979@gmail.com>
2017-09-13 20:27:16 +0100

    Extract out each of the functions from the catamorphism, as they're getting a bit too big

--- src/main/java/com/writeoncereadmany/minimalang/typechecking/TypeChecker.java
+++ src/main/java/com/writeoncereadmany/minimalang/typechecking/TypeChecker.java
@@ -1,9 +1,9 @@
 package com.writeoncereadmany.minimalang.typechecking;
 
-import co.unruly.control.pair.Maps;
 import co.unruly.control.pair.Pair;
 import co.unruly.control.result.Result;
 import com.writeoncereadmany.minimalang.ast.expressions.Expression;
+import com.writeoncereadmany.minimalang.ast.expressions.Expression.Catamorphism;
 import com.writeoncereadmany.minimalang.typechecking.types.FunctionType;
 import com.writeoncereadmany.minimalang.typechecking.types.InterfaceType;
 import com.writeoncereadmany.minimalang.typechecking.types.NamedType;
@@ -11,11 +11,10 @@ import com.writeoncereadmany.minimalang.typechecking.types.ObjectType;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 import static co.unruly.control.Lists.successesOrFailures;
 import static co.unruly.control.pair.Maps.entry;
-import static co.unruly.control.pair.Maps.mapOf;
+import static co.unruly.control.pair.Maps.toMap;
 import static co.unruly.control.result.Introducers.ifType;
 import static co.unruly.control.result.Match.matchValue;
 import static co.unruly.control.result.Resolvers.split;
@@ -30,46 +29,80 @@ import static java.util.stream.Collectors.toList;
 
 public interface TypeChecker {
 
-    static Expression.Catamorphism<Result<Type, List<TypeError>>, Types> typeChecker(final NamedType numberType, final NamedType stringType, final NamedType successType) {
-        return new Expression.Catamorphism<>(
-            contextFree(stringLiteral -> success(stringType)),
-            contextFree(numberLiteral -> success(numberType)),
-            (variable, type, context) -> type.either(
-                s -> Pair.of(success(successType), context.withVariable(variable, s)),
-                f -> Pair.of(failure(f), context)
-            ),
-            usingContext((variable, context) -> context.typeOf(variable)),
-            contextFree(fields -> {
-                Pair<List<Pair<String, Type>>, List<Pair<String, List<TypeError>>>> fieldsWhichExist = fields.entrySet()
-                        .stream()
-                        .<Result<Pair<String, Type>, Pair<String, List<TypeError>>>>map(entry -> entry.getValue().either(
-                            type -> success(entry(entry.getKey(), type)),
-                            typeErrors -> failure(entry(entry.getKey(), typeErrors))
-                        )).collect(split());
-                if(fieldsWhichExist.right.isEmpty()) {
-                    return success(new ObjectType(fieldsWhichExist.left.stream().collect(Maps.toMap())));
-                } else {
-                    return failure(fieldsWhichExist.right.stream().map(Pair::right).flatMap(List::stream).collect(toList()));
-                }
-            }),
-            usingContext((type, field, context) -> type
-                .then(attempt(context::resolve))
-                .then(attempt(t -> matchValue(t,
-                    ifType(InterfaceType.class, it -> it.getField(field))
-                ).otherwise(f -> failure(singletonList(new TypeError(format("Type %s is not an object type", t)))))))
-            ),
-            // this is a pretty difficult case
-            contextFree((params, body) -> null),
-            // this one's quite complicated too
-            usingContext((function, args, cata, types) -> successesOrFailures(args)
-                .either(
-                    arguments -> function.then(attempt(t ->
-                        matchValue(t,
-                            ifType(FunctionType.class, f -> f.returnType(arguments, types))
-                        ).otherwise(obj -> failure(singletonList(new TypeError(format("Type %s is not callable", obj))))))),
-                    failures -> failure(failures.stream().flatMap(List::stream).collect(toList())))
-            ),
-            contextFree(expressions -> null)
+    static Catamorphism<Result<Type, List<TypeError>>, Types> typeChecker(final NamedType numberType, final NamedType stringType, final NamedType successType) {
+        return new Catamorphism<>(
+            stringLiteral(stringType),
+            numberLiteral(numberType),
+            variableDeclaration(successType),
+            variable(),
+            objectLiteral(),
+            access(),
+            functionExpression(),
+            functionCall(),
+            group()
         );
+    }
+
+    static Expression.Interpreter<String, Result<Type, List<TypeError>>, Types> stringLiteral(NamedType stringType) {
+        return contextFree(stringLiteral -> success(stringType));
+    }
+
+    static Expression.Interpreter<String, Result<Type, List<TypeError>>, Types> numberLiteral(NamedType numberType) {
+        return contextFree(stringLiteral -> success(numberType));
+    }
+
+    static Expression.BiInterpreter<String, Result<Type, List<TypeError>>, Result<Type, List<TypeError>>, Types> variableDeclaration(NamedType successType) {
+        return (variable, type, context) -> type.either(
+            s -> Pair.of(success(successType), context.withVariable(variable, s)),
+            f -> Pair.of(failure(f), context)
+        );
+    }
+
+    static Expression.Interpreter<String, Result<Type, List<TypeError>>, Types> variable() {
+        return usingContext((variable, context) -> context.typeOf(variable));
+    }
+
+    static Expression.Interpreter<Map<String, Result<Type, List<TypeError>>>, Result<Type, List<TypeError>>, Types> objectLiteral() {
+        return contextFree(fields -> {
+            Pair<List<Pair<String, Type>>, List<Pair<String, List<TypeError>>>> fieldsWhichExist = fields.entrySet()
+                .stream()
+                .<Result<Pair<String, Type>, Pair<String, List<TypeError>>>>map(entry -> entry.getValue().either(
+                    type -> success(entry(entry.getKey(), type)),
+                    typeErrors -> failure(entry(entry.getKey(), typeErrors))
+                )).collect(split());
+            if(fieldsWhichExist.right.isEmpty()) {
+                return success(new ObjectType(fieldsWhichExist.left.stream().collect(toMap())));
+            } else {
+                return failure(fieldsWhichExist.right.stream().map(Pair::right).flatMap(List::stream).collect(toList()));
+            }
+        });
+    }
+
+    static Expression.BiInterpreter<Result<Type, List<TypeError>>, String, Result<Type, List<TypeError>>, Types> access() {
+        return usingContext((type, field, context) -> type
+            .then(attempt(context::resolve))
+            .then(attempt(t -> matchValue(t,
+                ifType(InterfaceType.class, it -> it.getField(field))
+            ).otherwise(f -> failure(singletonList(new TypeError(format("Type %s is not an object type", t)))))))
+        );
+    }
+
+    static Expression.BiInterpreter<List<String>, Expression, Result<Type, List<TypeError>>, Types> functionExpression() {
+        return contextFree((params, body) -> null);
+    }
+
+    static Expression.TriInterpreter<Result<Type, List<TypeError>>, List<Result<Type, List<TypeError>>>, Catamorphism<Result<Type, List<TypeError>>, Types>, Result<Type, List<TypeError>>, Types> functionCall() {
+        return usingContext((function, args, cata, types) -> successesOrFailures(args)
+            .either(
+                arguments -> function.then(attempt(t ->
+                    matchValue(t,
+                        ifType(FunctionType.class, f -> f.returnType(arguments, types))
+                    ).otherwise(obj -> failure(singletonList(new TypeError(format("Type %s is not callable", obj))))))),
+                failures -> failure(failures.stream().flatMap(List::stream).collect(toList())))
+        );
+    }
+
+    static Expression.Interpreter<List<Result<Type, List<TypeError>>>, Result<Type, List<TypeError>>, Types> group() {
+        return contextFree(expressions -> null);
     }
 }
