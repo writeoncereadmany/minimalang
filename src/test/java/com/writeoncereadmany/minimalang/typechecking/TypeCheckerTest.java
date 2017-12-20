093e5dca41787b5647734a7406c9e284aa4f03f8
tomj <tarjohnson1979@gmail.com>
2017-12-20 13:25:12 +0000

    Minor formatting changes

--- src/test/java/com/writeoncereadmany/minimalang/typechecking/TypeCheckerTest.java
+++ src/test/java/com/writeoncereadmany/minimalang/typechecking/TypeCheckerTest.java
@@ -48,7 +48,7 @@ public class TypeCheckerTest {
     private final Type showableImpl = new ObjectType(mapOf(entry("show", new ConcreteFunctionType(emptyList(), string))));
     private final Type printType = new ConcreteFunctionType(asList(showable), successType);
 
-    private final Expression.Catamorphism<Result<Type, List<TypeError>>, Types> cata = TypeChecker.typeChecker(
+    private final Expression.Catamorphism<Result<Type, List<TypeError>>, Types> typeChecker = TypeChecker.typeChecker(
             number,
             string,
             successType);
@@ -63,7 +63,7 @@ public class TypeCheckerTest {
     @Test
     public void canAssignStringToStringVariable() {
         Program program = compiler.compile("@String name is \"Tom\"");
-        Pair<Result<Type, List<TypeError>>, Types> result = program.run(cata, types);
+        Pair<Result<Type, List<TypeError>>, Types> result = program.run(typeChecker, types);
 
         assertThat(result.left, isSuccessOf(successType));
     }
@@ -71,7 +71,7 @@ public class TypeCheckerTest {
     @Test
     public void cannotAssignNumberToStringVariable() {
         Program program = compiler.compile("@String name is 42");
-        Pair<Result<Type, List<TypeError>>, Types> result = program.run(cata, types);
+        Pair<Result<Type, List<TypeError>>, Types> result = program.run(typeChecker, types);
 
         assertThat(result.left, isFailureOf(singleError("Cannot assign DataType{name='Number'} to DataType{name='String'}")));
     }
@@ -79,7 +79,7 @@ public class TypeCheckerTest {
     @Test
     public void canAddTwoNumbers() {
         Program program = compiler.compile("2:plus[4]");
-        Pair<Result<Type, List<TypeError>>, Types> result = program.run(cata, types);
+        Pair<Result<Type, List<TypeError>>, Types> result = program.run(typeChecker, types);
 
         assertThat(result.left, isSuccessOf(number));
     }
@@ -87,7 +87,7 @@ public class TypeCheckerTest {
     @Test
     public void canAddThreeNumbers() {
         Program program = compiler.compile("2:plus[4]:plus[6]");
-        Pair<Result<Type, List<TypeError>>, Types> result = program.run(cata, types);
+        Pair<Result<Type, List<TypeError>>, Types> result = program.run(typeChecker, types);
 
         assertThat(result.left, isSuccessOf(number));
     }
@@ -95,15 +95,17 @@ public class TypeCheckerTest {
     @Test
     public void cannotAddStringToNumber() {
         Program program = compiler.compile("2:plus[\"Hello, World!\"]");
-        Pair<Result<Type, List<TypeError>>, Types> result = program.run(cata, types);
+        Pair<Result<Type, List<TypeError>>, Types> result = program.run(typeChecker, types);
 
-        assertThat(result.left, isFailureOf(singleError("Cannot assign DataType{name='String'} to DataType{name='Number'}")));
+        assertThat(result.left, isFailureOf(singleError(
+            "Cannot assign DataType{name='String'} to DataType{name='Number'}"
+        )));
     }
 
     @Test
     public void canConcatenateTwoStrings() {
         Program program = compiler.compile("\"Hello, \":concat[\"World!\"]");
-        Pair<Result<Type, List<TypeError>>, Types> result = program.run(cata, types);
+        Pair<Result<Type, List<TypeError>>, Types> result = program.run(typeChecker, types);
 
         assertThat(result.left, isSuccessOf(string));
     }
@@ -111,7 +113,7 @@ public class TypeCheckerTest {
     @Test
     public void canConcatenateThreeStrings() {
         Program program = compiler.compile("\"Yabba, \":concat[\"Dabba, \"]:concat[\"Doo!\"]");
-        Pair<Result<Type, List<TypeError>>, Types> result = program.run(cata, types);
+        Pair<Result<Type, List<TypeError>>, Types> result = program.run(typeChecker, types);
 
         assertThat(result.left, isSuccessOf(string));
     }
@@ -119,7 +121,7 @@ public class TypeCheckerTest {
     @Test
     public void canDefineObjectsWithAnnotatedTypesWhenTypesMatch() {
         Program program = compiler.compile("point is { @Number x : 2, @Number y : 3 }");
-        Pair<Result<Type, List<TypeError>>, Types> result = program.run(cata, types);
+        Pair<Result<Type, List<TypeError>>, Types> result = program.run(typeChecker, types);
 
         assertThat(result.left, isSuccessOf(successType));
     }
@@ -127,11 +129,12 @@ public class TypeCheckerTest {
     @Test
     public void cannotDefineObjectsWithAnnotatedTypesWhenTypesDoNotMatch() {
         Program program = compiler.compile("point is { @String x : 2, @Success y : 3 }");
-        Pair<Result<Type, List<TypeError>>, Types> result = program.run(cata, types);
+        Pair<Result<Type, List<TypeError>>, Types> result = program.run(typeChecker, types);
 
         assertThat(result.left, isFailureOf(errors(
             "Cannot assign DataType{name='Number'} to DataType{name='String'}",
-            "Cannot assign DataType{name='Number'} to DataType{name='Success'}")));
+            "Cannot assign DataType{name='Number'} to DataType{name='Success'}"
+        )));
     }
 
     @Test
@@ -139,7 +142,7 @@ public class TypeCheckerTest {
         Program program = compiler.compile(String.join("\n",
                 "point is { x : 2, y : 3 }",
                 "point:x:plus[point:y]"));
-        Pair<Result<Type, List<TypeError>>, Types> result = program.run(cata, types);
+        Pair<Result<Type, List<TypeError>>, Types> result = program.run(typeChecker, types);
 
         assertThat(result.left, isSuccessOf(number));
     }
@@ -149,7 +152,7 @@ public class TypeCheckerTest {
         Program program = compiler.compile(String.join("\n",
                 "point is { x : \"Hello\", y : \"World\" }",
                 "point:x:plus[point:y]"));
-        Pair<Result<Type, List<TypeError>>, Types> result = program.run(cata, types);
+        Pair<Result<Type, List<TypeError>>, Types> result = program.run(typeChecker, types);
 
         assertThat(result.left, isFailureOf(singleError("Type String has no such field plus")));
     }
@@ -157,7 +160,7 @@ public class TypeCheckerTest {
     @Test
     public void supportsPolymorphicFunctionTakingString() {
         Program program = compiler.compile("print[\"Hello, World!\"]");
-        Pair<Result<Type, List<TypeError>>, Types> result = program.run(cata, types);
+        Pair<Result<Type, List<TypeError>>, Types> result = program.run(typeChecker, types);
 
         assertThat(result.left, isSuccessOf(successType));
     }
@@ -165,7 +168,7 @@ public class TypeCheckerTest {
     @Test
     public void supportsPolymorphicFunctionTakingNumber() {
         Program program = compiler.compile("print[12]");
-        Pair<Result<Type, List<TypeError>>, Types> result = program.run(cata, types);
+        Pair<Result<Type, List<TypeError>>, Types> result = program.run(typeChecker, types);
 
         assertThat(result.left, isSuccessOf(successType));
     }
@@ -173,7 +176,7 @@ public class TypeCheckerTest {
     @Test
     public void rejectsPolymorphicFunctionTakingObjectWithoutShow() {
         Program program = compiler.compile("print[{ x: 12, y: 14 }]");
-        Pair<Result<Type, List<TypeError>>, Types> result = program.run(cata, types);
+        Pair<Result<Type, List<TypeError>>, Types> result = program.run(typeChecker, types);
 
         assertThat(result.left, isFailureOf(singleError("Object has no such field show")));
     }
@@ -181,7 +184,7 @@ public class TypeCheckerTest {
     @Test
     public void rejectsPolymorphicFunctionTakingObjectWithNonFunctionShow() {
         Program program = compiler.compile("print[{ show: \"Cheese!\"}]");
-        Pair<Result<Type, List<TypeError>>, Types> result = program.run(cata, types);
+        Pair<Result<Type, List<TypeError>>, Types> result = program.run(typeChecker, types);
 
         assertThat(result.left, isFailureOf(singleError("Cannot assign an object type to a function type")));
     }
